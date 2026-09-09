869216329d78ce21daf2e0f1ef97f345f6ab9443
Vadim Filippov <100411976+Blackpoint133@users.noreply.github.com>
2026-09-09 02:39:05 -0700

    fix: support Sprite2D leg visuals

--- Game/scripts/player/modules/placeholder_legs.gd
+++ Game/scripts/player/modules/placeholder_legs.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var standing_visual: ColorRect = $StandingVisual
-@onready var crouching_visual: ColorRect = $CrouchingVisual
+@onready var standing_visual: CanvasItem = $StandingVisual
+@onready var crouching_visual: CanvasItem = $CrouchingVisual
 
 func set_crouching(crouching: bool) -> void:
 	standing_visual.visible = not crouching
